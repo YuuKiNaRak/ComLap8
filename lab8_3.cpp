03be40bed2de89a790403e3b5271a9673fff5ef4
ํYuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2021-12-03 16:51:06 +0700

    Complete lap8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    if(x == 'A'){
+        return 'Z';
+    }
+    if(x <= 'Z' && x > 'A'){
+        char y = x-1;
+        return y;
+    }else{
+        char n= '0';
+        return n;
+    
 }
 
 int main(){
